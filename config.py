aacd8d680a22012b7ca6dff19699efea74615a09
root <filip9322@gmail.com>
2022-05-26 00:33:06 +0900

    Connection with DB success, create and get recipes :ghost:

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 from pydantic import BaseSettings
+import motor.motor_asyncio
+from pydantic import BaseModel, Field
+from bson import ObjectId
+from typing import Optional
 
 
 class Settings(BaseSettings):
@@ -6,6 +10,67 @@ class Settings(BaseSettings):
     env_file_encoding = 'utf-8'
     app_name: str = "NLPGuide"
     admin_email: str
+    mongodb_url: str
 
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
+
+
+# Database Connection
+client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_url)
+db = client.nlpguide
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class RecipeModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    recipe_cousine: str = Field(...)
+    picture: str = Field(...)
+    ingredients: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Frito Pie",
+                "recipe_cousine": "western",
+                "picture": "https://i0.wp.com/thegoodlifefrance.com/wp-content/uploads/2018/04/provencal-tomatoes.jpg?ssl=1",
+                "ingredients": "1 pound ground beef, 2 cups shredded cheddar cheese",
+            }
+        }
+
+class UpdateStudentModel(BaseModel):
+    name: Optional[str]
+    recipe_cousine: Optional[str]
+    picture: Optional[str]
+    ingredients: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Frito Pie",
+                "recipe_cousine": "western",
+                "picture": "https://i0.wp.com/thegoodlifefrance.com/wp-content/uploads/2018/04/provencal-tomatoes.jpg?ssl=1",
+                "ingredients": "1 pound ground beef, 2 cups shredded cheddar cheese",
+            }
+        }
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from functools import lru_cache
-from fastapi import FastAPI
-from config import settings
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from config import settings, db, RecipeModel
 from enum import Enum
-from typing import Union
+from typing import Union, List
 import dotenv
 
 app = FastAPI()
@@ -12,16 +14,16 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
-@app.get("/")
+@app.get("/", response_description="Hello world")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}" , response_description="Say hello to name")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 # predefined values test /models/alexnet
-@app.get("/models/{model_name}")
+@app.get("/models/{model_name}", response_description="List all models")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return{"model_name" : model_name, "message": "Deep Learning FTW!"}
@@ -30,7 +32,7 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 # query params example /items/foo?short=False
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_description="List all items")
 async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
     item = {"item_id": item_id}
     if q:
@@ -40,9 +42,26 @@ async def read_item(item_id: str, q: Union[str, None] = None, short: bool = Fals
     return item
 
 # trying to use .env files records /info
-@app.get("/info")
+@app.get("/info", response_description="List info test")
 async def info():
     return {
          "app_name": settings.app_name,
-         "admin_email": settings.admin_email
+         "admin_email": settings.admin_email,
+         "mongodb_url": settings.mongodb_url
     }
+
+# check DB for recipes
+@app.get(
+    "/recipes", response_description="List all recipes", response_model=List[RecipeModel]
+)
+async def list_recipes():
+    recipes = await db["recipes"].find().to_list(1000)
+    return recipes
+
+# create recipe into DB
+@app.post("/recipe", response_description="Add new student", response_model=RecipeModel)
+async def create_recipe(recipe: RecipeModel = Body(...)):
+    recipe = jsonable_encoder(recipe)
+    new_recipe = await db["recipes"].insert_one(recipe)
+    created_recipe = await db["recipes"].find_one({"_id": new_recipe.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_recipe)
